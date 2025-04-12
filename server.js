26d8fa3b67552f47109c357ff0bfb2fa01ad8cc9
Amirul <amirulzaimal24@gmail.com>
2025-04-13 00:00:15 +0800

    Updated server.js with auth routes

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => res.send('Hello World!'));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 require('dotenv').config(); // only needed locally
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))

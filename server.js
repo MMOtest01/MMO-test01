3455bb24c86697188774ea4e319c26205809c74a
Amirul <amirulzaimal24@gmail.com>
2025-04-13 00:48:05 +0800

    Updated server.js with auth routes

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => res.send('Hello World!'));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 require('dotenv').config(); // only needed locally
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
@@ -27,7 +27,7 @@ require('./models/Character');
 
 require('dotenv').config();
 // const express = require('express');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const cors = require('cors');
 
 // const express = require('express');

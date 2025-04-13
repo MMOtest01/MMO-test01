40489e2ef31e0709e3a1c66070b31897a8f27a6c
Amirul <amirulzaimal24@gmail.com>
2025-04-13 11:38:22 +0800

    Fix: added type dependencies and styled-components support

--- server.js
+++ server.js
@@ -25,9 +25,8 @@ app.get('/', (req, res) => {
 });
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+mongoose.connect(process.env.MONGO_URI)
+
 }).then(() => {
   console.log('MongoDB connected');
 }).catch((err) => {

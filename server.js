b5b672b2a0af0b62e8fd667651564a90f63fb537
Amirul <amirulzaimal24@gmail.com>
2025-04-13 11:44:03 +0800

    Fix: added type dependencies and styled-components support

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
 
-}).then(() => {
+.then(() => {
   console.log('MongoDB connected');
 }).catch((err) => {
   console.error("MongoDB connection error:", err);
